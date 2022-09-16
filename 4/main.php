47b4d5bbc50bb2ad37b8d355a7886740f4d3bff8
Razawaky <viniciusduarte564@gmail.com>
2022-09-16 10:48:50 -0300

    87tvfvgiy

--- 4/main.php
+++ 4/main.php
@@ -2,10 +2,15 @@
 
 $idade = $_POST['idd'];
 
-switch($idade) {
-    case 1:
-        echo "Não pode";
-    break;
-    
+//não eleitor (abaixo de 16 anos)eleitor obrigatório ( entre 18 e 65 anos)eleitor facultativo ( entre 16 e 18 anos e maior de 65 anos)
+
+if($idade < 16) {
+    echo "Não eleitor";
+}
+if($idade >= 18 && $idade $idade <= 65) {
+   echo "eleitor obrigatório";
+}
+if($idade >= 16 && $idade <= 18 || $idade > 65 ) {
+    echo "eleitor facultativo";
 }
 
